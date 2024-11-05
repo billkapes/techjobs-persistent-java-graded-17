952e690466d767f23b9f496de8b1e69ce8ff3c0d
billkapes <kapes15@yahoo.com>
2024-11-04 20:32:17 -0600

    working on task 2. did up to create a skill controller

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String location;
+
+    public Employer() {}
+
+    public @NotEmpty @Size(min = 2, max = 100) String getLocation() {
+        return location;
+    }
+
+    public void setLocation(@NotEmpty @Size(min = 2, max = 100) String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(min = 1, max = 255)
+    private String description;
+
+    public @Size(min = 1, max = 255) String getDescription() {
+        return description;
+    }
+
+    public Skill() {}
+
+    public void setDescription(@Size(min = 1, max = 255) String description) {
+        this.description = description;
+    }
 }
