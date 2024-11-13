254246fda6811a55d89e6b78ae036ded616d6e5c
billkapes <kapes15@yahoo.com>
2024-11-13 00:21:56 -0600

    fixed query

--- queries.sql
+++ queries.sql
@@ -14,5 +14,5 @@ TABLE job;
 --Part 4
 SELECT * FROM skill
 INNER JOIN job_skills ON skill.id = job_skills.skills_id
-WHERE job_skills.job_id IS NOT NULL
+WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
